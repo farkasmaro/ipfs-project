a953904015224b6962dad30e63c2438f2f586187
farkasmaro <farkasmaro@gmail.com>
2022-01-18 16:50:48 +0000

    update storage contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,16 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract SimpleStorage {
-  uint storedData;
+  string ipfsHash;
+  //encrypted location of the file
 
-  function set(uint x) public {
-    storedData = x;
+  function set(string memory x) public {
+    //change value of hash
+    ipfsHash = x;
   }
 
-  function get() public view returns (uint) {
-    return storedData;
+  function get() public view returns (string memory) {
+    //retrieve value of hash
+    return ipfsHash;
   }
 }
